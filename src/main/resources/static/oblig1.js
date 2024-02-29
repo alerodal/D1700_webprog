97ba2bf10c96963b355ebf56adecaf7827596de7
Aleksander Rødal <alrod3388@oslomet.no>
2024-02-29 21:34:20 +0100

    Updated oblig1.js

--- src/main/resources/static/oblig1.js
+++ src/main/resources/static/oblig1.js
@@ -1,8 +1,9 @@
 
-const billettArray = [];
-function kjopBillett(){
-    const antall = parseInt(document.getElementById("antall").value);
+    const billettArray = [];
+
+    function kjopBillett() {
     const film = document.getElementById("velg").value;
+    const antall = document.getElementById("antall").value;
     const fornavn = document.getElementById("fornavn").value;
     const etternavn = document.getElementById("etternavn").value;
     const telefonnr = document.getElementById("telefonnr").value;
@@ -11,96 +12,94 @@ function kjopBillett(){
     let isValid = true;
 
     const tlfNrRegex = /^\d{3}[\s-]?\d{2}[\s-]?\d{3}$/;
-    const epostRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
+    const epostRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if (film == "Velg film her"){
-        document.getElementById("filmMangel").textContent="Må velge en film";
-        isValid=false;
-    }else{
-        document.getElementById("filmMangel").textContent="";
-    }
-
-    if (!antall || isNaN(antall)) {
-        document.getElementById("antallMangel").textContent ="Må skrive inn et gyldig tall";
-        isValid=false;
-    } else {
-        document.getElementById("antallMangel").textContent ="";
-    }
-
-
-    if (!fornavn){
-        document.getElementById("fornavnMangel").textContent="Må skrive noe inn i fornavnet";
-        isValid=false;
-    }else {
-        document.getElementById("fornavnMangel").textContent ="";
-    }
-
-
-    if(!etternavn){
-        document.getElementById("etternavnMangel").textContent="Skriv inn noe i etternavn";
-        isValid=false;
-    }else{
-        document.getElementById("etternavnMangel").textContent="";
-    }
-
-
-    if (!telefonnr){
-        document.getElementById("telefonMangel").textContent="Må skrive inn telefonnr";
-        isValid=false;
-    }else if(!tlfNrRegex.test(telefonnr)){
-        document.getElementById("telefonMangel").textContent="Skriv inn gyldig telefonnr";
-    }else{
-        document.getElementById("telefonMangel").textContent="";
-    }
-
-
-    if (!epost){
-        document.getElementById("epostMangel").textContent ="Må skrive noe inn i epost";
-        isValid = false;
-    }else if(!epostRegex.test(epost)){
-        document.getElementById("epostMangel").textContent="Skriv inn gyldig epost";
-    }else{
-        document.getElementById("epostMangel").textContent="";
-    }
-
-    if(!isValid){
-        return;
-    }
-
-    const person = {
-        fornavn : fornavn,
-        etternavn : etternavn,
-        telefonnr : telefonnr,
-        epost : epost,
-        antall : antall,
-        film : film
-    };
-    billettArray.push(person);
-
-
-    let ut = "";
-    ut+= "<table><tr>"+
-        "<th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th>"+"</tr>";
-
-    for (let personInfo of billettArray){
-        ut+="<tr>";
-        ut+="<td>"+personInfo.film+"</td><td>"+personInfo.antall+"</td><td>"+personInfo.fornavn+"</td><td>"+personInfo.etternavn+"</td><td>"+personInfo.telefonnr+"</td><td>"+
-            personInfo.epost+"</td>";
-        ut+="</tr>";
-    }
-    document.getElementById("ut").innerHTML=ut;
-
-
-    document.getElementById("fornavn").value="";
-    document.getElementById("etternavn").value="";
-    document.getElementById("telefonnr").value="";
-    document.getElementById("epost").value="";
-    document.getElementById("antall").value = "";
-    document.getElementById("velg").value="";
+    if (film === "nøytral") {
+    document.getElementById("filmMangel").innerHTML = "Må velge en film";
+    isValid = false;
+} else {
+    document.getElementById("filmMangel").innerHTML = "";
 }
 
+    if (antall === "") {
+    document.getElementById("antallMangel").innerHTML = "Må skrive inn et gyldig tall";
+    isValid = false;
+} else if (Number(antall) >= 1) {
+    document.getElementById("antallMangel").innerHTML = "";
+} else {
+    document.getElementById("antallMangel").innerHTML = "Velg gyldig antall";
+    isValid = false;
+}
 
-function slettBillett(){
-    billettArray.length=0;
-    document.getElementById("ut").innerHTML="";
+    if (fornavn === "") {
+    document.getElementById("fornavnMangel").innerHTML = "Må skrive noe inn i fornavnet";
+    isValid = false;
+} else {
+    document.getElementById("fornavnMangel").innerHTML = "";
+}
+
+    if (etternavn === "") {
+    document.getElementById("etternavnMangel").innerHTML = "Skriv inn noe i etternavn";
+    isValid = false;
+} else {
+    document.getElementById("etternavnMangel").innerHTML = "";
+}
+
+    if (telefonnr === "") {
+    document.getElementById("telefonMangel").innerHTML = "Må skrive inn telefonnr";
+    isValid = false;
+} else if (!tlfNrRegex.test(telefonnr)) {
+    document.getElementById("telefonMangel").innerHTML = "Skriv inn gyldig telefonnr";
+    isValid = false;
+} else {
+    document.getElementById("telefonMangel").innerHTML = "";
+}
+
+    if (epost === "") {
+    document.getElementById("epostMangel").innerHTML = "Må skrive noe inn i epost";
+    isValid = false;
+} else if (!epostRegex.test(epost)) {
+    document.getElementById("epostMangel").innerHTML = "Skriv inn gyldig epost";
+    isValid = false;
+} else {
+    document.getElementById("epostMangel").innerHTML = "";
+}
+
+    if (!isValid) {
+    return;
+}
+
+    const person = {
+    film: film,
+    antall: antall,
+    fornavn: fornavn,
+    etternavn: etternavn,
+    telefonnr: telefonnr,
+    epost: epost,
+};
+    billettArray.push(person);
+    visBillett();
+
+    function visBillett() {
+    let ut = "<table><tr><th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th></tr>";
+    for (let personInfo of billettArray) {
+    ut += "<tr>";
+    ut += "<td>" + personInfo.film + "</td><td>" + personInfo.antall + "</td><td>" + personInfo.fornavn + "</td><td>" + personInfo.etternavn + "</td><td>" + personInfo.telefonnr + "</td><td>" +
+    personInfo.epost + "</td>";
+    ut += "</tr>";
+}
+    document.getElementById("ut").innerHTML = ut;
+}
+
+    document.getElementById("fornavn").value = "";
+    document.getElementById("etternavn").value = "";
+    document.getElementById("telefonnr").value = "";
+    document.getElementById("epost").value = "";
+    document.getElementById("antall").value = "";
+    document.getElementById("velg").value = "nøytral";
+}
+
+    function slettBillett() {
+    billettArray.length = 0;
+    document.getElementById("ut").innerHTML = "";
 }
